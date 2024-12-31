edbe9490b50ed4bef9201fa142bcd3f084e6c07d
Anna Sas <anna.sas.dev@gmail.com>
2024-12-31 12:36:15 +0100

    Feat: AddOneTimeDataSeeder overload to setup seeder directly

--- src/CodeOfChaos.Types.DataSeeder/ServiceCollectionExtensions.cs
+++ src/CodeOfChaos.Types.DataSeeder/ServiceCollectionExtensions.cs
@@ -2,6 +2,8 @@
 // Imports
 // ---------------------------------------------------------------------------------------------------------------------
 using CodeOfChaos.Types;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Extensions.Hosting;
 
 // ReSharper disable once CheckNamespace
 namespace Microsoft.Extensions.DependencyInjection;
@@ -10,10 +12,28 @@ namespace Microsoft.Extensions.DependencyInjection;
 // ---------------------------------------------------------------------------------------------------------------------
 public static class ServiceCollectionExtensions {
     public static IServiceCollection AddOneTimeDataSeeder<TDataSeeder>(this IServiceCollection services)
-        where TDataSeeder : class, IDataSeederService
+        where TDataSeeder : class, IDataSeederService, IHostedService
         => services.AddHostedService<TDataSeeder>();
 
     public static IServiceCollection AddOneTimeDataSeeder<TDataSeeder>(this IServiceCollection services, Func<IServiceProvider, TDataSeeder> implementationFactory)
-        where TDataSeeder : class, IDataSeederService
+        where TDataSeeder : class, IDataSeederService, IHostedService
         => services.AddHostedService(implementationFactory);
+
+    public static IServiceCollection AddOneTimeDataSeeder<TDataSeeder>(this IServiceCollection services, Action<TDataSeeder> configure)
+        where TDataSeeder : class, IDataSeederService, IHostedService {
+        
+        services.TryAddEnumerable(
+            ServiceDescriptor.Singleton<IHostedService, TDataSeeder>(provider
+                => {
+                    var seeder = ActivatorUtilities.CreateInstance<TDataSeeder>(provider);
+                    configure(seeder);
+                    return seeder;
+                }
+            )
+        );
+        return services;
+    }
+
+    public static IServiceCollection AddOneTimeDataSeeder(this IServiceCollection services, Action<OneTimeDataSeederService> configure)
+        => AddOneTimeDataSeeder<OneTimeDataSeederService>(services, configure);
 }
